4d33c24c3e387d10b39d3943db2e84f1d659b49b
ailtontmarques <ailton.marques@gmail.com>
2023-10-23 21:03:00 -0300

    adding UserDataBuilder method

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { UserEntity, UserProps } from '../../user.entity';
 import { before } from 'node:test';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
 
 describe('UserEntity unit tests', () => {
 
@@ -8,11 +9,7 @@ describe('UserEntity unit tests', () => {
   let sut:UserEntity;
 
   beforeEach(() => {
-    props = {
-        name: faker.person.fullName(),
-        email: faker.internet.email(),
-        password: faker.internet.password()
-      }
+  props = UserDataBuilder({});
 
     sut = new UserEntity(props);
   });
