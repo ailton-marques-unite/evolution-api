9b17c6110cef5bf810362f4906ae71af7995d5ac
ailton-marques-unite <ailton.marques@grupounite.com.br>
2025-10-03 10:27:16 -0300

    feat:added chat controller  messages endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { AppService } from './app.service';
 import { InstanceModule } from './instances/instance.module';
 import { WebhookModule } from './webhook/webhook.module';
 import { SendMessageModule } from './send-message/send-message.module';
+import { ChatControllerModule } from './chat-controller/chat-controller.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), 
     InstanceModule, 
     WebhookModule, 
-    SendMessageModule,
+    SendMessageModule, ChatControllerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
