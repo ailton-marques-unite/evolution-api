99f94fd9163df5e367632a03cf53fbcaf6c5c955
ailton-marques-unite <ailton.marques@grupounite.com.br>
2025-10-03 15:35:28 -0300

    feat:added profile settings messages endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,13 @@ import { InstanceModule } from './instances/instance.module';
 import { WebhookModule } from './webhook/webhook.module';
 import { SendMessageModule } from './send-message/send-message.module';
 import { ChatControllerModule } from './chat-controller/chat-controller.module';
+import { ProfileSettingsModule } from './profile-settings/profile-settings.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), 
     InstanceModule, 
     WebhookModule, 
-    SendMessageModule, ChatControllerModule,
+    SendMessageModule, ChatControllerModule, ProfileSettingsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
